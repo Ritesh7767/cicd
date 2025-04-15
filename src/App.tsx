1fc2688507d5f9a8064e66ebee81015e4a0ac5b5
Ritesh7767 <ritesh776782@gmail.com>
2025-04-16 00:16:27 +0530

    first message

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const handleClick = () => setCount((prev: number) => prev + 1)
+
+  return (
+    <>
+      <h1>Counter value :- {count}</h1>
+      <button onClick={handleClick} >+</button>
+    </>
+  )
+}
+
+export default App
